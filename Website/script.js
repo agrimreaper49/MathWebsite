ea81fc589aad64b2e5c241b36db99aac2e6d713f
Agrim Sharma <56984222+agrimreaper49@users.noreply.github.com>
2020-04-02 15:42:21 -0400

    added files

---
+++ Website/script.js
@@ -0,0 +1,11 @@
+var num1;
+var num2;
+var guess;
+var answer;
+var response;
+
+$(document).ready(new function() {
+    num1 = document.getElementById("num1");
+    num1.innerHTML("hey");
+    num2 = document.getElementById("num2");
+})
