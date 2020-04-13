a57808b557b44269df732dc1d0219cb5a673d1b0
Agrim Sharma <56984222+agrimreaper49@users.noreply.github.com>
2020-04-13 14:10:57 -0400

    Update script.js

--- Website/script.js
+++ Website/script.js
@@ -4,8 +4,31 @@ var guess;
 var answer;
 var response;
 
-$(document).ready(new function() {
+$(document).ready( function() {
     num1 = document.getElementById("num1");
-    num1.innerHTML("hey");
     num2 = document.getElementById("num2");
+    guess = document.getElementById("inputfield");
+    response = document.getElementById("reponse");
+    
+    $("#submitbutton").click(function() {
+        checkAnswers();
+    })
+    setNumbers();
 })
+
+function setNumbers () {
+    num1.innerHTML = Math.floor(Math.random() * 10) + 1;
+    num2.innerHTML = Math.floor(Math.random() * 10) + 1;
+}
+function checkAnswers () {
+    var n1  = parseInt(num1.innerHTML);
+    var n2  = parseInt(num2.innerHTML);
+    
+    answer = n1 * n2;
+    
+    if ( parseInt(guess.value) == answer ){
+        response.innerHTML = "yes!"; 
+    }else{
+        response.innerHTML = "no!"
+    }
+}
